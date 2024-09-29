b3840ce5ef1c1f8f73975bf77d84f08cc6af492e
Abdullah <145125629+AhibAli863@users.noreply.github.com>
2024-09-29 13:03:38 +0500

    script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ loop: true
 
 const header = document.querySelector("header");
 window.addEventListener("scroll" , function(){
-    header.classList.toggle("sticky" , this.window.scrollY > 0);
+    header.classList.toggle("sticky" , this.window.scrollY > 10);
 
 }); 
 
@@ -25,10 +25,53 @@ navbar.onclick = function(){
 
 };
 
-
 var typed = new Typed('.text-3', {
     strings: ['Frontend Developer!','Web Designer','Web Developer.'],
     typeSpeed: 100,
     backSpeed: 60,
 loop: true
 });
+
+const tl = gsap.timeline();
+
+tl.from(".logo",{
+    opacity:0,
+    duration: 1,
+    x: -150,
+    stagger:1
+});
+
+
+tl.from(".navbar .list",{
+    opacity: 0,
+    duration: 0.7,
+    y: -30,
+    stagger:0.3,
+});
+
+tl.from(".left .text-2 p",{
+    opacity:0,
+    duration: 0.5,
+    x:-1100,
+});
+
+
+
+gsap.from("#about .content .left-cont img",{
+    opacity:0,
+    x:-1500,
+    duration:1,
+    delay:1,
+    rotate:"360",
+    scale:"0.2",
+    scrollTrigger:{
+        trigger:"#about",
+        scroller:"body",
+        start:"top 0%",
+        end:"top -20%",
+        scrub:1,
+        pin:true
+
+
+    }
+})
